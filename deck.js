def196365ed33f7f6b1102ae3f0df76ee012eb91
Stephanie Sohns <ssohns@gmail.com>
2020-02-25 16:43:13 -0700

    bit changes

--- deck.js
+++ deck.js
@@ -144,13 +144,13 @@ pushSelectedCards(selectedCard) {
 
 
 
-  shuffle() {
+  shuffle()
     //for (var i = this.cards.length - 1; i > 0; i--) {
     //var j = Math.floor(Math.random() * (i + 1));
     // [this.cards[i].matchInfo, this.cards[j].matchInfo] =
     // [this.cards[j].matchInfo, this.cards[i].matchInfo];
   // }randomize placement of all cards (this.cards)
-   }
+
 
 
 
@@ -162,14 +162,14 @@ pushSelectedCards(selectedCard) {
     //no match = two cards flip back over after two seconds
 
 
-  moveToMatched(matchedCardsInstance) {
+  moveToMatched(matchedCardsInstance) 
     this.matchedCards.push(matchedCardsInstance);
     // this.matchedCards.push(this.selectedCards);
     // this.selectedCards[0].matched = true;
     // this.selectedCards[1].matched = true;
     // // console.log(this.matchedCards);
     this.selectedCards = [];
-  }
+
 
     //when a user matches two like cards
     //those cards are pushed to the matched array
@@ -193,4 +193,3 @@ pushSelectedCards(selectedCard) {
   }
   //push new cards to cards array
   }
-}
